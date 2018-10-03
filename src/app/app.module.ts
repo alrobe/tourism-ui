b4d5c0308f7efc9042ef453487dd9d97372a8d01
unknown <denikin_santos@hotmail.com>
2018-10-02 20:13:12 -0400

    creacion del componente Destinos Turisticos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaqueteTuristicoComponent } from './components/paquete-turistico/paquete-turistico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaqueteTuristicoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/paquete-turistico/paquete-turistico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paquete-turistico',
+  templateUrl: './paquete-turistico.component.html',
+  styleUrls: ['./paquete-turistico.component.css']
+})
+export class PaqueteTuristicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
