25465c290f94066e29d70c03798da7251e492c2f
Alfredo.RObles <alf.asper@gmail.com>
2018-10-06 19:08:39 -0400

    creacion de la estructura para agencia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: 'agency',
+    loadChildren: './pages/agency/agency.module#AgencyModule'
+  }
 ]
 
 @NgModule({
