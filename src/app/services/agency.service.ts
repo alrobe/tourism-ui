69f25ca8578818abe6dbb327509f66d6ec89a2f8
unknown <orlandolopez.lt@gmail.com>
2018-10-11 07:47:56 -0400

    servicio para la agencia

--- src/app/services/agency.service.ts
+++ src/app/services/agency.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import {Agency} from './../model/agency';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgencyService {
 
-  constructor() { }
+baseUrl: string = 'http://localhost:8080/agency';
+
+  constructor(private http: HttpClient) {
+      console.log('service working!');
+   }
+
+   createAgency(agency: Agency){
+      return this.http.post(this.baseUrl, agency);
+  }
 }
