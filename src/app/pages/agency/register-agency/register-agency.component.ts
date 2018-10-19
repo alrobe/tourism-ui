49f2e3a4f68230a39f862630547c0e6468b496b9
Alfredo Robles Bernabe <alfredo.robles@fundacion-jala.org>
2018-10-19 14:20:05 -0400

    creacion del formulario para registrar agencias

--- src/app/pages/agency/register-agency/register-agency.component.ts
+++ src/app/pages/agency/register-agency/register-agency.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AgencyService } from '../../../services/agency.service';
 
 @Component({
   selector: 'app-register-agency',
   templateUrl: './register-agency.component.html'
 })
 export class RegisterAgencyComponent implements OnInit {
-
-  constructor() { }
+  
+  agency: FormGroup;
+  constructor(private fb: FormBuilder, private agencyService: AgencyService) { }
 
   ngOnInit() {
+    this.agency = this.fb.group({
+      name: ['', [Validators.required]],
+      phone: ['', Validators.required],
+      email: ['', [Validators.required]],
+      webPage: ['', [Validators.required]]
+    });
   }
-
 }
