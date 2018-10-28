6731ca6c1090c88445c32e30a64f1ff8ac3bcb9a
Alfredo Rocha Aquino <alfredorochaaquino10@gmail.com>
2018-10-28 18:58:23 -0400

    se aumento informacion en el modelo de destinoturistico

--- src/app/model/tourist_destiny.ts
+++ src/app/model/tourist_destiny.ts
@@ -3,12 +3,22 @@ import { Province } from './province'
 export class TouristDestiny {
     name: string;
     coordinates: string;
-    
+    lodging: string;
+    internet: string;
+    weather: string;
+    feeding: string;
+    timeTravel: string;
+
     province: Province;
 
-    constructor(name: string, coordinates: string, province: Province) {
+    constructor(name: string, coordinates: string,lodging: string; internet: string; weather: string; feeding: string; timeTravel: string; province: Province) {
         this.name = name;
         this.province = province;
-        this.coordinates = coordinates
+        this.coordinates = coordinates;
+        this.lodging = lodging;
+        this.internet = internet;
+        this.weather = weather;
+        this.feeding = feeding;
+        this.timeTravel = timeTravel;
     }
 }
