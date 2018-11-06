91a643e3f4afba31e373b99ce06a42b4c09ae449
pacha2880 <33531552+pacha2880@users.noreply.github.com>
2018-11-06 02:19:15 -0400

    crear componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CreateDestinyComponent } from './pages/tourist_destiny/create-destiny/create-destiny.component';
+import { CircuitoVistaDestinosComponent } from './pages/circuito-vista-destinos/circuito-vista-destinos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateDestinyComponent
+    CreateDestinyComponent,
+    CircuitoVistaDestinosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/circuito-vista-destinos/circuito-vista-destinos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-circuito-vista-destinos',
+  templateUrl: './circuito-vista-destinos.component.html',
+  styleUrls: ['./circuito-vista-destinos.component.css']
+})
+export class CircuitoVistaDestinosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
