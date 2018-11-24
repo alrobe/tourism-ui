17eb968a1cbea7abad3f5231fc465777a39ea3c3
dljcurci <dljcurci@gmail.com>
2018-11-24 13:07:36 -0500

    Working on Destination by id

---
+++ src/app/pages/tourist_destiny/destiny-by-id/destiny-by-id.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-destiny-by-id',
+  templateUrl: './destiny-by-id.component.html',
+  styleUrls: ['./destiny-by-id.component.css']
+})
+export class DestinyByIdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
