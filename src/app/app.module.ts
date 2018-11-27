2990492acafb29006ae0cd3081f2aff213980e7b
orlandoloto <orlandolopez.lt@gmail.com>
2018-11-27 07:45:44 -0400

    page home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { CircuitoVistaDestinosComponent } from './pages/circuito-vista-destinos/
 import { AgencyService } from './services/agency.service';
 import { TouristDestinyService } from './services/touristDestiny.service';
 import { PagerService } from './services/pager.service';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { PagerService } from './services/pager.service';
     CreateDestinyComponent,
     PaqueteTuristicoCreateComponent,
     PaqueteTuristicoEditComponent,
-    CircuitoVistaDestinosComponent
+    CircuitoVistaDestinosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
